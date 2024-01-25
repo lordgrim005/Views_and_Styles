e9b5dab9c6f50f24b50972f9ec1cd97db7ab453b
Archie Daffa <archiedaffa5@gmail.com>
2024-01-25 22:49:15 +0700

    Finish

---
+++ app/src/main/java/com/example/views_and_styles/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.views_and_styles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
